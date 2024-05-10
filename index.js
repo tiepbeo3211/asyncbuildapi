ea3166bf09c0be40326145e1735283934abe268d
tiepbeo3211 <marein0106nam1311@outlook.com>
2024-05-10 18:32:30 +0700

    commit

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-function insertionSort(arr) {
-  for (let i = 1; i < arr.length; i++) {
-    let key = arr[i];
-    let j = i - 1;
-    while (j >= 0 && arr[j] > key) {
-      arr[j + 1] = arr[j];
-      j--;
+function numIslands(grid) {
+  if (!grid || grid.length === 0) return 0;
+  const m = grid.length;
+  const n = grid[0].length;
+  let islands = 0;
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (grid[i][j] === "1") {
+        islands++;
+        dfs(grid, i, j);
+      }
     }
-    arr[j + 1] = key;
   }
-  return arr;
+  return islands;
+}
+function dfs(grid, i, j) {
+  const m = grid.length;
+  const n = grid[0].length;
+  if (i < 0 || j < 0 || i >= m || j >= n || grid[i][j] === "0") return;
+  grid[i][j] = "0";
+  dfs(grid, i + 1, j);
+  dfs(grid, i - 1, j);
+  dfs(grid, i, j + 1);
+  dfs(grid, i, j - 1);
 }
